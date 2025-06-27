906ed294f5f92be7dad7a336f4a07bc3406417d7
MANOJ PALI <manojpali.x@outlook.com>
2025-06-27 17:25:41 +0530

    this is testing and trial

--- mysite/settings.py
+++ mysite/settings.py
@@ -59,7 +59,7 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
 
-# STATIC_URL = '/static/'
+STATIC_URL = '/static/'
 # STATIC_ROOT = BASE_DIR / 'staticfiles'
 # STATICFILES_DIRS = [BASE_DIR / 'static']
 
