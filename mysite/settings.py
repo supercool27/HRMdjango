b5400f76a6ed0dac5aa698354e105a9e2fcca765
MANOJ PALI <manojpali.x@outlook.com>
2025-06-27 17:22:30 +0530

    this is live server tested

--- mysite/settings.py
+++ mysite/settings.py
@@ -59,8 +59,15 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
 
-STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+# STATIC_URL = '/static/'
+# STATIC_ROOT = BASE_DIR / 'staticfiles'
+# STATICFILES_DIRS = [BASE_DIR / 'static']
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_collect')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
