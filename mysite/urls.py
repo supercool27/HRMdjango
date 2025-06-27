d27250adf1f0011f0c598b9b75e26545700cd3d6
MANOJ PALI <manojpali.x@outlook.com>
2025-06-27 14:27:55 +0530

    first initial commit for development

---
+++ mysite/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("<h1>Welcome to Railway Django Deployment!</h1>")
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', home),
+]
