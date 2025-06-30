81e3ec028504395d02a69138326f3725a65b5961
MANOJ PALI <manojpali.x@outlook.com>
2025-06-30 10:53:48 +0530

    Fix: Removed name

--- mysite/urls.py
+++ mysite/urls.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 from django.urls import path, include
 
 def home(request):
-    return HttpResponse("<h1>Welcome to Railway Django Deployment Mr. Manoj Pali Welcome in home !</h1>")
+    return HttpResponse("<h1>Welcome to Railway Django Deployment Welcome in home !</h1>")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
