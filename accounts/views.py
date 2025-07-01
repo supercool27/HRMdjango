120f88439c67a028ac40341c994299a79c21eee3
MANOJ PALI <manojpali.x@outlook.com>
2025-07-01 14:44:09 +0530

    login and signup paged is linked and working fine checked

--- accounts/views.py
+++ accounts/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def login_view(request):
     return render(request, 'accounts/login.html')
+
+def signup_view(request):
+    return render(request, 'accounts/signup.html')
