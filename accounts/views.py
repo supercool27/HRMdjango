0c2d36916296389d284f3e65af34804ef6260f6e
MANOJ PALI <manojpali.x@outlook.com>
2025-07-04 15:58:31 +0530

    Addition: requird login is added

--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,29 @@
 from django.shortcuts import render
 from django.contrib import messages
 from django.contrib.auth import get_user_model
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
 
 def login_view(request):
-    return render(request, 'accounts/login.html')
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
 
+        user = authenticate(request, username=username, password=password)
+        
+        if user is not None:
+            login(request, user)
+            messages.success(request, "🎉 Logged in successfully!")
+            return redirect('dashboard')  # your dashboard url name
+        else:
+            messages.error(request, "⚠️ Invalid username or password.")
+
+    return render(request, "accounts/login.html")
+
+@login_required
 def dashboard_view(request):
-    return render(request, 'accounts/dashboard.html')\
+    return render(request, 'accounts/dashboard.html')
 
 def signup_view(request):
     return render(request, 'accounts/signup.html')
