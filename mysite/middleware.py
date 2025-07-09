9eff14726944a5df245263a9ff77860ba7ac972d
MANOJ PALI <manojpali.x@outlook.com>
2025-07-09 15:06:51 +0530

    Fix: Middleware indentation

--- mysite/middleware.py
+++ mysite/middleware.py
@@ -13,7 +13,7 @@ class LoginRequiredMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
 
-   def __call__(self, request):
+    def __call__(self, request):
         if not request.user.is_authenticated:
             path = request.path_info
             if not any(path.startswith(url) for url in EXEMPT_URLS):
